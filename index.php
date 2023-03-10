deb7322d08f67bbc3c029b12f8328b34ee31c06a
SbandAid <santiagocuria1711@gmail.com>
2023-03-09 23:42:06 -0300

    Comenzando con la idea

--- index.php
+++ index.php
@@ -32,28 +32,31 @@ if (isset($_POST['submit'])) {
   <meta charset="utf-8">
   <title>Programa</title>
   <link rel="stylesheet" href="./style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
 </head>
-  <body data-spy="scroll" data-offset="0" data-target="#navigation">
-
+  <body class="fondo">
   	<div class="contenedor">
 		<form action="" method="POST" class="login-email">
-		<p>Iniciar Sesión</p>
-			<div class="input-group">
+			<h1 class="titulo-principal">Iniciar Sesión</h1>
+			<div class="inputs">
 				<label for="email">Usuario</label>
-				<input type="email" placeholder="Email" name="email" value="<?php echo $_POST['email']; ?>" class='anadir' required>
+				<input type="email" placeholder="Email" name="email" value="<?php echo $_POST['email']; ?>" class='anad	ir' required>
 			</div>
-			<div class="input-group">
+			<div class="inputs">
 				<label for="password">Contraseña</label>
 				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" class='anadir'  required>
 			</div>
-			<div class="input-group">
-				<button name="submit" class="submit">Iniciar Sesión</button>
+			<div class="inputs">
+				<button name="submit" class="submit btn btn-primary btn-sm">Iniciar <span>Sesión</span></button>
 			</div>
 		</form>
 	</div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
     </body>
 </html>
 
--- main.php
+++ main.php
@@ -19,23 +19,29 @@ if (!isset($_SESSION['username'])) {
         <meta content="" name="description">
         <link rel="stylesheet" type="text/css" href="style.scss" />
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="style.css">
     </head>
-    <body data-spy="scroll" data-offset="0" data-target="#navigation">
-        <form action="/resumen.php">
-            <input type="submit" value="Resumen Diario" />
-        </form>
-        <form action="/pagos/agregar_pago.php">
-            <input type="submit" value="Agregar Pago" />
-        </form>
-        <form action="/socios/socios.php">
-            <input type="submit" value="Ver Socios" />
-        </form>
-        <form action="/pagos/historial_pagos.php">
-            <input type="submit" value="Ver Pagos" />
-        </form>
-        <form action="/socios/agregar_socio.php">
-            <input type="submit" value="Agregar Socios" />
-        </form>
-        <a href="logout.php">Cerrar Sesión</a>
+    <body class="fondo">
+        <aside class="menu-lateral">
+            <div class="icono">
+                <i><!--icono del programa--></i>
+            </div>
+            <form action="/resumen.php">
+                <input type="submit" value="Resumen Diario" class="menu-lateral--btn"/>
+            </form>
+            <form action="/pagos/agregar_pago.php" class="menu-lateral--form">
+                <input type="submit" value="Agregar Pago" class="menu-lateral--btn"/>
+            </form>
+            <form action="/socios/socios.php">
+                <input type="submit" value="Ver Socios" class="menu-lateral--btn"/>
+            </form>
+            <form action="/pagos/historial_pagos.php">
+                <input type="submit" value="Ver Pagos" class="menu-lateral--btn"/>
+            </form>
+            <form action="/socios/agregar_socio.php">
+                <input type="submit" value="Agregar Socios" class="menu-lateral--btn"/>
+            </form>
+            <a href="logout.php">Cerrar Sesión</a>
+        </aside>
     </body>
 </html>
